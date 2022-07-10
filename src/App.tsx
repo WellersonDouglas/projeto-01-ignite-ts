3f1c99271f946c1cba08c6807b68a19d8cada87e
WellersonDouglas <wellersondouglasvr@gmail.com>
2022-07-10 13:45:29 +0100

    new file:   src/vite-env.d.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
-import styles from "./App.module.css";
+import styles from './App.module.css';
 
 import './styles/global.css';
 
